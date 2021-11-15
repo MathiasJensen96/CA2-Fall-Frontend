ea7d3690c57f107230ed0097595e8d7eaf94aade
Mathias Jensen <cph-mj839@chpbusiness.dk>
2021-11-15 11:58:04 +0100

    with pic

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -3,6 +3,7 @@ const Dog = (props) => {
         <div>
           <h2>Dog</h2>
           <p>{props.message}</p>
+          <img class="picture" src={props.message}></img>
         </div>
       );
 }
